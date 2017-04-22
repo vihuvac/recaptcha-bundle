59bd198e8177874f68ae6fbfb8e510b4012610e2
Víctor Hugo Valle Castillo <victouk@gmail.com>
2017-04-22 16:16:04 -0300

    Cleaning up unnecessary parent method.

--- Form/Type/VihuvacRecaptchaType.php
+++ Form/Type/VihuvacRecaptchaType.php
@@ -12,7 +12,6 @@
 namespace Vihuvac\Bundle\RecaptchaBundle\Form\Type;
 
 use Vihuvac\Bundle\RecaptchaBundle\Locale\LocaleResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\AbstractType;
@@ -130,14 +129,6 @@ class VihuvacRecaptchaType extends AbstractType
                 )
             )
         );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getParent()
-    {
-        return TextType::class ?: "text";
     }
 
     /**
